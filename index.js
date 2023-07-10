c3f822f380992bae88824e18e696963a5a3d4339
Raymond Schweers <raymond@hetwebbureau.nl>
2023-07-10 16:23:14 +0200

    feat(next) enable plugin and resolve nextjs links

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ module.exports = {
   extends: [
     './configs/eslint.js',
     './configs/react.js',
-    './configs/next.js',
     './configs/typescript.js',
     './configs/prettier.js',
+    './configs/next.js'
   ]
 };
